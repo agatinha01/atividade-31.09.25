e19be05a74ad9b3f2cc59064d4d39e71cd2dab55
Agatha <agathavitoria23109@gmail.com>
2025-09-30 16:39:48 -0300

    Add files via upload

---
+++ atividade_1/Program.cs
@@ -0,0 +1,13 @@
+﻿int num = 15;
+int num2 = 35;
+
+var A = num;
+int B = num2;
+
+Console.WriteLine(A);
+Console.WriteLine(B);
+Console.WriteLine("Digite qualquer rápido para mudar a ordem das variaveais.");
+Console.ReadLine();
+
+Console.WriteLine(B);
+Console.WriteLine(A);
