14e811504d854ae6c8a6bd235ab251c3fe0cd749
Agatha <agathavitoria23109@gmail.com>
2025-09-30 17:03:34 -0300

    Add files via upload

---
+++ atividade_4/Program.cs
@@ -0,0 +1,6 @@
+﻿int idade, ano;
+
+Console.WriteLine("Em que ano você nasceu?");
+idade = int.Parse(Console.ReadLine());
+ano = (2025 - idade) * 365;
+Console.WriteLine($"Você tem {ano} dias de vida");
