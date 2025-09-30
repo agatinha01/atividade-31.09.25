9cd0c542f40f048f0efb08e634bae1d69d37371c
Agatha <agathavitoria23109@gmail.com>
2025-09-30 16:43:03 -0300

    Add files via upload

---
+++ atividade_2/Program.cs
@@ -0,0 +1,10 @@
+﻿int num;
+int resultado;
+
+Console.WriteLine("Digite um número.");
+
+num = int.Parse(Console.ReadLine());
+
+resultado = num - 1;
+
+Console.WriteLine($"O antessesor de {num} é {resultado}");
