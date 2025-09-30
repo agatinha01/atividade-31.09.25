0760c58403467d4369bb24a5a069fd6b4e953c8c
Agatha <agathavitoria23109@gmail.com>
2025-09-30 16:53:05 -0300

    Add files via upload

---
+++ atividade_3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+int Base;
+int Altura;
+
+Console.WriteLine("Digite a base do seu retangulo.");
+
+Base = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Diga a altura do seu retângulo.");
+Altura = int.Parse(Console.ReadLine());
+
+int area = Base * Altura;
+
+Console.WriteLine($"A área do seu retângulo é {area}");
