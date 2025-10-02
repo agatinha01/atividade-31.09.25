fc7fdd78733aba660eafd02f3cae5ed04a02b134
Agatha <agathavitoria23109@gmail.com>
2025-10-02 16:00:33 -0300

    Add files via upload

---
+++ atividade 9/Program.cs
@@ -0,0 +1,17 @@
+﻿double apple;
+double pagar;
+
+Console.WriteLine("voce vai comprar quantas maçãs");
+apple = double.Parse(Console.ReadLine());
+
+if (apple < 12)
+{
+    pagar = apple * 1.30;
+    Console.WriteLine($"voce ira pagar R${pagar:F2}");
+}
+else
+{
+
+    pagar = apple * 1.00;
+Console.WriteLine($"voce ira pagar R${pagar:F2}");
+}
