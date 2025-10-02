50532667910b9db02c811fc9d849fd8022ea5984
Agatha <agathavitoria23109@gmail.com>
2025-10-02 15:14:35 -0300

    Add files via upload

---
+++ atividade 7/Program (2).cs
@@ -0,0 +1,13 @@
+﻿int batata;
+
+Console.WriteLine("digite um numero");
+batata = int.Parse(Console.ReadLine());
+
+if (batata > 10)
+{
+    Console.WriteLine("seu numero é maior que 10");
+}
+else
+{
+    Console.WriteLine("seu numero  não é maior que 10");
+}
---
+++ atividade 7/Program.cs
@@ -0,0 +1,14 @@
+﻿int taylan;
+
+
+Console.WriteLine("digite um numero");
+taylan = int.Parse(Console.ReadLine());
+
+if (taylan > 0)
+{
+    Console.WriteLine("o numero é positivo");
+}
+else
+{
+    Console.WriteLine("o numero é negativo");
+}
