f0fdc7b467e4a8c73186e57760a2575a85bfa839
Agatha <agathavitoria23109@gmail.com>
2025-10-02 16:13:51 -0300

    Add files via upload

---
+++ atividade 10/Program.cs
@@ -0,0 +1,16 @@
+﻿double nota1;
+double nota2;
+
+Console.WriteLine("digite a sua primeira nota.");
+nota1 = double.Parse(Console.ReadLine());
+Console.WriteLine("digite a sua segunda nota.");
+nota2 = double.Parse(Console.ReadLine());
+
+if (nota1+nota2 > 6)
+{
+    Console.WriteLine("voce foi aprovado.");
+}
+else
+{
+    Console.WriteLine("voce foi reprovado.");
+}
