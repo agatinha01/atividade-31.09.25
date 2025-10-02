c6f87dc33e53a868a5f03a64ba8203c7839e7416
Agatha <agathavitoria23109@gmail.com>
2025-10-02 16:22:08 -0300

    Add files via upload

---
+++ atividade 11/Program.cs
@@ -0,0 +1,15 @@
+﻿int atual, nascimento;
+Console.WriteLine("qual seu ano de nascimento?");
+nascimento = int.Parse(Console.ReadLine());
+
+Console.WriteLine("qual seu ano atual?");
+atual = int.Parse(Console.ReadLine());
+
+if (nascimento > 2007)
+{
+    Console.WriteLine("voce nao precisa votar ainda.");
+}
+else
+{
+  Console.WriteLine("voce precisa votar.");
+}
