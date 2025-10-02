fa5837d83138270a1a722eb6b84c4a5fa206bd23
Agatha <agathavitoria23109@gmail.com>
2025-10-02 16:33:44 -0300

    Add files via upload

---
+++ atividade 12/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+int valor;
+int valor2;
+
+Console.WriteLine("digite o primeiro numero.");
+valor = int.Parse(Console.ReadLine());
+Console.WriteLine("digite o segundo numero.");
+valor2 = int.Parse(Console.ReadLine());
+
+if (valor > valor2)
+{
+    Console.WriteLine($"{valor} e maior que {valor2}");
+}
+else
+{
+    Console.WriteLine($"{valor2} e maior que {valor}");
+}
