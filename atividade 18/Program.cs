e8fc69d53ff9a58cd21d6b8167c6c320902bd155
Agatha <agathavitoria23109@gmail.com>
2025-10-07 15:55:46 -0300

    Add files via upload

---
+++ atividade 18/Program.cs
@@ -0,0 +1,18 @@
+﻿double num = 1;
+double media = 1;
+
+for (double i = 0; i < media; i++)
+{
+    if (num != 0)
+    {
+        Console.WriteLine("digite um numero.");
+        num = double.Parse(Console.ReadLine());
+
+        if (num != 0)
+        {
+            media = num / 2;
+            Console.WriteLine(media);
+        }
+    }
+
+}
