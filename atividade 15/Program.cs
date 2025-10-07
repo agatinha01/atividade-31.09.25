500ff2c772622595ebda5bb5cb7e847b02434032
Agatha <agathavitoria23109@gmail.com>
2025-10-07 14:23:46 -0300

    Add files via upload

---
+++ atividade 15/Program.cs
@@ -0,0 +1,13 @@
+﻿int n;
+int num = 0;
+int valor = 0;
+
+Console.WriteLine("digite qualquer numero.");
+n = int.Parse(Console.ReadLine());
+
+for (int i = 1000; i < 2000; i++)
+{
+    num++;
+    valor = num / 11;
+}
+Console.WriteLine(valor);
