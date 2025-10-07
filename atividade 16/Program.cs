3a2ab38b5379c0d46c6f178818850a1f31dc8a61
Agatha <agathavitoria23109@gmail.com>
2025-10-07 14:08:22 -0300

    Add files via upload

---
+++ atividade 16/Program.cs
@@ -0,0 +1,10 @@
+﻿int n;
+int resultado = 0;
+
+Console.WriteLine("digite um numero e vamos dizer o fatoral dele.");
+n = int.Parse(Console.ReadLine());
+for (int i = 1; i < n; i++)
+{
+    resultado = (n - 1) * n;
+}
+Console.WriteLine (resultado);
