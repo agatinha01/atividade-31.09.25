a170ea3736b98e75296a1c84f3610f4f4dc821d4
Agatha <agathavitoria23109@gmail.com>
2025-10-07 14:16:05 -0300

    Add files via upload

---
+++ atividade 14/Program.cs
@@ -0,0 +1,13 @@
+﻿int n;
+int num = 0;
+int valor = 0;
+
+Console.WriteLine("digite um valor.");
+n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    num++;
+    valor = num;
+    Console.WriteLine(valor);
+}
