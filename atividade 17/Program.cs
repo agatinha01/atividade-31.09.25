5aed10a7bdc5277ef85dbbc88da3bace6e292ceb
Agatha <agathavitoria23109@gmail.com>
2025-10-07 14:51:03 -0300

    Add files via upload

---
+++ atividade 17/Program.cs
@@ -0,0 +1,38 @@
+﻿int n;
+int n2;
+int n3;
+int n4;
+int n5;
+int nega = 0;
+
+Console.WriteLine("digite 5 numeros.");
+n = int.Parse(Console.ReadLine());
+n2  = int.Parse(Console.ReadLine());
+n3 = int.Parse(Console.ReadLine());
+n4 = int.Parse(Console.ReadLine());
+n5 = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"{n}\n{n2}\n{n3}\n{n4}\n{n5}\n");
+
+if (n < 0)
+{
+    nega = nega + 1;
+}
+if (n2 < 0)
+{ 
+    nega = nega + 1;
+}
+if (n3 < 0)
+{
+    nega = nega + 1;
+}
+if (n4 < 0)
+{
+    nega = nega + 1;
+}
+if (n5 < 0)
+{
+    nega = nega + 1;
+}
+
+Console.WriteLine($"{nega} sao negativos.");
